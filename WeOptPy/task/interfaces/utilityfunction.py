6fb81ff1642e8dfd82b7f6b36774f0b1cde12fbf
klemen@atom.feri.um.si <klemen.berkovic1@um.si>
2020-09-16 13:52:07 +0200

    Removed unused packages

--- WeOptPy/task/interfaces/utilityfunction.py
+++ WeOptPy/task/interfaces/utilityfunction.py
@@ -3,9 +3,7 @@
 """Implementation of benchmarks utility function."""
 
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
+
 
 __all__ = ['UtilityFunction']
 
@@ -74,41 +72,6 @@ class UtilityFunction:
 			Callable[[Union[list, numpy.ndarray], Dict[str, Any]], float]: Fitness funciton.
 		"""
 		return self.function()
-
-	def plot2d(self):
-		r"""Plot 2D graph."""
-		pass
-
-	def __2dfun(self, x, y, f):
-		r"""Calculate function value.
-
-		Args:
-			x (float): First coordinate.
-			y (float): Second coordinate.
-			f (Callable[[Union[int, float, list, numpy.ndarray], Dict[str, Any]], float]): Evaluation function.
-
-		Returns:
-			float: Calculate functional value for given input
-		"""
-		return f([x, y])
-
-	def plot3d(self, scale=0.32):
-		r"""Plot 3d scatter plot of benchmark function.
-
-		Args:
-			scale (float): Scale factor for points.
-		"""
-		fig = plt.figure()
-		ax = Axes3D(fig)
-		func = self.function()
-		Xr, Yr = np.arange(self.lower, self.upper, scale), np.arange(self.lower, self.upper, scale)
-		X, Y = np.meshgrid(Xr, Yr)
-		Z = np.vectorize(self.__2dfun)(X, Y, func)
-		ax.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3)
-		ax.contourf(X, Y, Z, zdir='z', offset=-10, cmap=cm.coolwarm)
-		ax.set_xlabel('X')
-		ax.set_ylabel('Y')
-		ax.set_zlabel('Z')
 
 
 # vim: tabstop=3 noexpandtab shiftwidth=3 softtabstop=3
